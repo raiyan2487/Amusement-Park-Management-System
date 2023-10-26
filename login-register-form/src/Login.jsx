b31dfef25a10e31ede3f6c4190b3a882c251b0d3
LamiaEmu <148773670+LamiaEmu@users.noreply.github.com>
2023-10-26 23:13:25 +0600

    updated login-register-form

--- login-register-form/src/Login.jsx
+++ login-register-form/src/Login.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 
 export const Login = (props) => {
-    const [email, setEmail] = useState('');
+    const [userID, setID] = useState('');
     const [pass, setPass] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(email);
+        console.log(userID);
     }
     
     return (
@@ -14,8 +14,8 @@ export const Login = (props) => {
             <h2>Login</h2>
             <form className="login-form" onSubmit={handleSubmit}>
                 
-                <label htmlFor="email">email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <label htmlFor="userID">userID</label>
+                <input value={userID} onChange={(e) => setID(e.target.value)}type="text" placeholder="your userID" id="email" name="email" />
                 
                 <label htmlFor="password">password</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
