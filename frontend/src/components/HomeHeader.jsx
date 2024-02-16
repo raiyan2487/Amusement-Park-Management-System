335d37440055145d6c93f1dc66fe5f48684849ad
LamiaEmu <148773670+LamiaEmu@users.noreply.github.com>
2024-02-17 02:46:03 +0600

    fixed home navbar

--- frontend/src/components/HomeHeader.jsx
+++ frontend/src/components/HomeHeader.jsx
@@ -3,13 +3,17 @@ import { Link } from "react-router-dom"
 
 export const Header = () => {
     return (
-        <nav id="header" className="bg-black text-white">
+        <nav id="header" className="bg-opacity-0 text-dark blue absolute z-10 flex justify-end inset-x-0">
             <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
 
                 <div className="logo-wrapper pl-4 flex items-center">
                     <Link to="/" className="toggleColor text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl">
                         <img src={imagination} alt="logo" className="w-40 h-20 object-cover"/>
                     </Link>
+                </div>
+
+                <div className="nav-package-wrapper flex items-center justify-between space-x-10">
+                    
                 </div>
 
                 <div className="flex items-center justify-center space-x-4">
@@ -21,7 +25,6 @@ export const Header = () => {
                         document.cookie = 'token=0'
                         window.location.href = 'http://localhost:3000/login'
                     }}>Sign Out</div>
-                    
                     <p>&nbsp;&nbsp;</p>
                 </div>
 
