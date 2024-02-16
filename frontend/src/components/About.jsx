be141a10411a71e0887442908213bd1662281a83
LamiaEmu <148773670+LamiaEmu@users.noreply.github.com>
2024-02-17 02:21:43 +0600

    added homepage

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,18 @@
+const About = () => {
+    return (
+        <div className="about">
+            <div className="bg-white">
+                <div className="p-24 grid grid-cols-auto-fit">
+                    <div className="">
+                        <h2 className="text-2xl font-medium">About Us</h2>
+                        <p className="text-lg">
+                        Welcome to Imagination, where the extraordinary becomes reality! Immerse yourself in a world of thrilling rides, enchanting gardens, and vibrant entertainment. Our commitment to safety ensures worry-free fun for all ages. At Imagination, create lasting memories as you experience the magic of limitless possibilities. Welcome to the adventure!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
